a0ee9321b1bfb54dd31924fd4cb773e9aed3e610
defud12345 <defud12345@gmail.com>
2023-07-30 18:27:54 +0700

    Mouse

--- Assets/Scripts/Verse/PlayerHandle/Mouse.cs
+++ Assets/Scripts/Verse/PlayerHandle/Mouse.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mouse : MonoBehaviour
+public class Mouse
 {
-    // Start is called before the first frame update
-    void Start()
+    public Mouse()
     {
-        
+        GetMouse = this;
+    }
+    public static Mouse GetMouse;
+    private void MouseUpdate()
+    {
+        if(GetMouse==null){return;}
+
+        currMousePos = Current.Camera.ScreenToWorldPoint(Input.mousePosition);
+        //currMousePos.z = Current.CameraFollow.target.transform.position.z; not yet
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private Vector2 currMousePos;
+    private Vector2 lastMousePos;
+
+    private bool isDragging = false;
+
+
+    public Vector3 GetMousePos
     {
+        get{return currMousePos;}
+    }
+    public enum MouseMode
+    {
+        Select,
+        Build,
         
     }
 }
